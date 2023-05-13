f11bcaa1495320b1d0ecdfee071e75d880d54e9c
Vijay A <vraravam@users.noreply.github.com>
2023-05-13 08:06:26 +0530

    make the square clikable

--- src/component/gameboard/Gameboard.js
+++ src/component/gameboard/Gameboard.js
@@ -1,6 +1,6 @@
 //create a gameboard component
 //import React
-import React from 'react';
+import React, {useEffect} from 'react';
 //import the Square component
 import Square from '../square/Square';
 //import the calculateWinner function
@@ -9,29 +9,39 @@ import Square from '../square/Square';
 import './Gameboard.css';
 //create a functional component called Gameboard that takes in props
 // how to give squares
-const Gameboard = ({ onClick }) => {
-    //create a 2d array of 6 rows and 7 columns
-    const squares = Array(6).fill(Array(7).fill(null));
-    //
-    // const [board, setBoard] = useState(squares);
-    // const [player, setPlayer] = useState(1);
+const Gameboard = ({  }) => {
+     const [squares, setSquares] = React.useState([]);
+     const [clicks, setClicks] = React.useState(1);
+
+     function initSquare() {
+         var i = 0;
+         //create a 2d array of 6 rows and 7 columns and fill it with object with properties i, background_color and i should be incremented by 1
+         const squares = Array(6).fill(null).map(() => Array(7).fill(null).map(() => ({i: ++i, backgroundColor: "white"})));
+          setSquares(squares);
+     }
+
+     useEffect(() => {
+         initSquare();
+    }, []);
 
 return (
-//Create a 6 x 7 matrix using the map function inside the gameboard component
-<div className="gameboard">
-    {squares.map((row,index) => (
-        <>
-        <div className='Row'>
-            {
-        row.map((square, i) => (
-        <Square key={index.toString()+i} value={square} onClick={() => onClick(i)} />
-        ))}
-        </div> 
-        <br></br>
-        </>
-    ))}
+//Create a 6 x 7 matrix using the map function inside the gameboard component using the squares array
+//Use the map function to create a row of 7 squares
+//Use the map function to create a square with a value of 0
+//Use the map function to create a square with a value of 1
+//Use the map function to create a square with a value of 2
+//Use the map function to create a square with a value of 3
 
-</div>
+        <div className="gameboard">
+            {squares.map((row, rowIndex) => (
+                <div key={rowIndex} className="row">
+                    {row.map((square, colIndex) => (
+                        <Square key={colIndex} value={square} setClick={setClicks} clicks={clicks} />
+                    ))}
+                </div>
+            ))}
+        </div>
+
 
 )
 }
--- src/component/square/Square.js
+++ src/component/square/Square.js
@@ -5,14 +5,28 @@ import React from 'react';
 import './square.css';
 //create a functional component called Square that takes in props
 
-const Square = ({ value, onClick }) => (
+const Square = ({ value, setClick, clicks }) => {
     //Use HTML to create a button
-    
+
+    const onClick = (value) => {
+        //when background color is not white return
+        if (value.backgroundColor !== "white") {
+            return;
+        }
+        if (clicks % 2 === 0) {
+            value.backgroundColor = "red";
+        } else {
+            value.backgroundColor = "yellow";
+        }
+        setClick(clicks + 1);
+    }
 
     // Use HTML to create a button that displays the value of the square
-    <button className="square" onClick={onClick}>
-        {value}
+    return     (<button id={`square-${value.i}`} style={{ backgroundColor: value.backgroundColor }} className="square" onClick={()=>{onClick(value)}}>
     </button>
-)
+);
+};
+
+
 //export the Square component
 export default Square;
